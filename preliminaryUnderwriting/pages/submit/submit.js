b681c2b6db300f6da777dd1f915e90ef28adb1ca
hushuaibo <979587457@qq.com>
2019-04-18 10:30:15 +0800

    change submit

--- preliminaryUnderwriting/pages/submit/submit.js
+++ preliminaryUnderwriting/pages/submit/submit.js
@@ -16,6 +16,15 @@ Page({
             index: e.detail.value
         })
     },
+    /*
+   *日期选择
+   */
+    bindDateChange(e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            date: e.detail.value
+        })
+    },
   /**
    * 生命周期函数--监听页面加载
    */
