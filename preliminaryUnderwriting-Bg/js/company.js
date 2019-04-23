aed003622f9e3f1f3323ef591bb3ff2f0110e573
lujunhui <lujunhui_c@163.com>
2019-04-23 11:41:28 +0800

    add form

---
+++ preliminaryUnderwriting-Bg/js/company.js
@@ -0,0 +1,11 @@
+//  添加
+$('.box_add').click(function(){
+    showModal();
+})
+// 点击关闭
+$('.oper_close').click(function(){
+    closeModal();
+})
+$('.sub_no').click(function(){
+    closeModal();
+})
